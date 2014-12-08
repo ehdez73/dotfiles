5650a94b184b948e84fa248e3ce163c324585c84
Ernesto Hdez <ehdez73@gmail.com>
2014-12-08 20:31:50 +0100

    fixed chrome

--- chrome/install.sh
+++ chrome/install.sh
@@ -4,7 +4,7 @@ if test ! $(which google-chrome)
 then
   echo "  Installing Google Chrome."
   sudo apt-get -y install libxss1 libappindicator1 libindicator7
-  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /tmp/google-chrome.deb
-  sudo dpkg -i /tmp/google-chrome.deb
-  google-chrome&
+  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
+  sudo dpkg -i google-chrome-stable_current_amd64.deb
+  rm google-chrome-stable_current_amd64.deb
 fi
