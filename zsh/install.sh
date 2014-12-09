c0cdb012ef6f68867c8b3d72e635a6d1a440a0e4
Ernesto Hernández <ehdez73@gmail.com>
2014-12-09 12:42:14 +0100

    Update install.sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,5 +3,5 @@ if test ! $(which zsh)
 then
 	sudo apt-get install zsh
 	curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-	chsh -s `which zsh`
+	chsh -s $(which zsh)
 fi
