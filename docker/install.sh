1be48f470ad90c174d8b6656239c1c4fd4d9ff85
Ernesto Hdez <ehdez73@gmail.com>
2015-02-10 19:01:21 +0100

    Docker & Fig

--- docker/install.sh
+++ docker/install.sh
@@ -1,8 +1,17 @@
-#!/bin/sh
-
+#!/bin/bash
 if test ! $(which docker)
 then
-  echo "  Installing docker."
-  sudo apt-get -y install docker.io
-  sudo usermod -aG docker $USER
+	echo -e "\e[33mInstalling Docker\e[39m"
+  	sudo apt-get -y install docker.io
+	sudo usermod -aG docker $USER
 fi
+
+if test ! $(which fig)
+then
+	echo -e "\e[33mInstalling Fig\e[39m"
+	sudo pip install -U fig
+fi
+
+sudo apt-get install dnsmasq
+
+
