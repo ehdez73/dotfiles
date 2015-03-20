7ca0f240693b5569d24a54ac00664ac13787a00a
Ernesto Hernandez <ehdez73@gmail.com>
2015-03-20 16:36:50 +0100

    update to Java 8u40 and set profile.d/java.sh

--- java/install.sh
+++ java/install.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-#if test ! $(which javac)
-#then
-  echo "Installing JDK."
+if test ! $(which javac)
+then
+  echo "Installing JDK. 8u40"
   wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u40-b26/jdk-8u40-linux-x64.tar.gz
-# sudo apt-get -y install libxss1 libappindicator1 libindicator7
-# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
-# sudo dpkg -i google-chrome-stable_current_amd64.deb
-# rm google-chrome-stable_current_amd64.deb
-
-#fi
+  tar zxvf jdk-8u40-linux-x64.tar.gz
+  sudo mkdir -p /opt/java/
+  sudo mv jdk1.8.0_40 /opt/java
+  sudo rm /opt/java/jdk
+  sudo ln -s /opt/java/jdk1.8.0_40  /opt/java/jdk
+  printf '%s\n%s\n' 'export JAVA_HOME=/opt/java/jdk' 'export PATH=$JAVA_HOME/bin:$PATH' >> /etc/profile.d/java.sh
+fi
 
