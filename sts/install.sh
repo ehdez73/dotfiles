8c964661743063423712bfd0bd5fe6609d4a29f0
Ernesto Hernandez <ehdez73@gmail.com>
2015-03-20 16:37:55 +0100

    Install Spring Tool Suite 3.6.4.RELEASE

---
+++ sts/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if test /opt/springsource/sts-bundle/
+then
+	echo "Found STS installation at /opt/springsource/sts-bundle"
+else
+	echo "Installing Spring Tool Suite"
+	wget http://dist.springsource.com/release/STS/3.6.4.RELEASE/dist/e4.4/spring-tool-suite-3.6.4.RELEASE-e4.4.2-linux-gtk-x86_64.tar.gz
+	tar zxvf spring-tool-suite-3.6.4.RELEASE-e4.4.2-linux-gtk-x86_64.tar.gz
+	sudo mkdir -p /opt/springsource
+	sudo mv sts-bundle /opt/springsource/.
+	ln -s $(pwd)/sts.desktop ~/.local/share/applications/sts.desktop
+fi
+
