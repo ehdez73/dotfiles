0946a880520bb831379467e052c7f916b0df9c6f
Ernesto Hernandez <ernesto.hernandez@medianet.es>
2015-06-03 09:54:34 +0200

    Add gitk to git installation

--- git/install.sh
+++ git/install.sh
@@ -3,5 +3,5 @@
 if test ! $(which meld)
 then
   echo "  Installing git."
-  sudo apt-get -y install git git-gui meld vim > /tmp/git-install.log
+  sudo apt-get -y install git git-gui gitk meld vim > /tmp/git-install.log
 fi
