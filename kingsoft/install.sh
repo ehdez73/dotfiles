d4d5f7d0e5c0e63ab867c0da7f1462907e16061e
Ernesto Hernandez <ernesto.hernandez@medianet.es>
2015-06-30 12:56:57 +0200

    Update Kingsoft WPS to 9.1.0.4961~a18p1_i386

--- kingsoft/install.sh
+++ kingsoft/install.sh
@@ -3,7 +3,7 @@
 if test ! $(which wps)
 then
 	echo "  Installing KingSoft"
-	wget http://kdl.cc.ksosoft.com/wps-community/download/a16/wps-office_9.1.0.4945~a16p3_i386.deb
+	wget http://kdl.cc.ksosoft.com/wps-community/download/a18/wps-office_9.1.0.4961~a18p1_i386.deb
 	sudo gdebi wps-office_9.1.0.4945~a16p3_i386.deb
 	rm wps-office_9.1.0.4945~a16p3_i386.deb
 
